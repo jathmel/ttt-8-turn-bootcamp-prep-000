df930823cd182efef7ec7729083f7b5b73a67c51
Jathmel Gordon <jathmel@gmail.com>
2018-04-16 14:55:25 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,8 @@
 def display_board(board)
-  puts "#{board[0]} | #{board[0]} | #{board[0]} "
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
   puts "-----------"
-  puts "#{board[0]} | #{board[0]} | #{board[0]} "
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
   puts "-----------"
-  puts "#{board[0]} | #{board[0]} | #{board[0]} "
+  puts " #{board[0]} | #{board[0]} | #{board[0]} "
 
 end
